8e5099ac1caa7cd808a1b1c9a82d52cc40dadf2e
PlaceHolderHere <holderhereinplace@gmail.com>
2026-03-20 20:11:41 +0800

    Added variables.js with basic variable types and functions

---
+++ variables.js
@@ -0,0 +1,32 @@
+// Declaration
+let username  // Variable named username with no value
+let age = 15
+
+// Datatypes
+let name = "Jeff" // String
+let height = 180 // Number
+let weight = 50.5 // Number
+let isOverEightteen = false // Boolean
+
+// Built In Type Functions
+// Boolean() -> returns whether an expression or variable is true
+// Convert other data types to booleans
+console.log(Boolean(10 > 9))
+console.log(Boolean(9 > 10))
+console.log(Boolean(0))
+console.log(Boolean("c"))
+console.log(Boolean(""))
+
+// Number() -> converts an input to a Number
+// returns NaN if it can't be converted
+console.log(Number(false))
+console.log(Number(true))
+console.log(Number(""))
+console.log(Number("a"))
+console.log(Number("Hi"))
+
+// String() -> converts an input to a String
+console.log(String(9))
+console.log(String(false))
+console.log(String(true))
+console.log(String(-1))

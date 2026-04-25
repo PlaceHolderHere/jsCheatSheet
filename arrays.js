50fd566ebc283d3f0c4384c2b395bc04197ae050
PlaceHolderHere <holderhereinplace@gmail.com>
2026-04-25 23:06:52 +0800

    Added arrays.js

---
+++ arrays.js
@@ -0,0 +1,10 @@
+let products = ["apples", "oranges"]
+console.log(products[0])  // 1st item in the array
+
+products[3] = "banana"  // Adding an item at any index
+console.log(products)
+
+products[2] = 1 // arrays can have multiple data types in them
+console.log(products)
+
+console.log(products.length)
